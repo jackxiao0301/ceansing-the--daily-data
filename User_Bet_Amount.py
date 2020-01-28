3d41e1fa047dc779420de4f2cc981604f6dab102
Jesse Pinkman <hot6523@gmail.com>
2020-01-28 10:41:52 +0800

    add progressbar

--- User_Bet_Amount.py
+++ User_Bet_Amount.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np 
 import datetime
+import time 
+import progressbar
 
 
 def open_csv(csv_date):
@@ -26,9 +28,14 @@ def cleansing_table(df, day):
 if __name__ == '__main__':
     begin = input('Input the begin date(Format:YYYY_MM_DD)')
     end = input('Input the end date(Format:YYYY_MM_DD):')
+    barbn = int(begin.split('_')[2])
+    baren = int(end.split('_')[2])
     begin = datetime.date(int(begin.split('_')[0]),int(begin.split('_')[1]),int(begin.split('_')[2]))
     end = datetime.date(int(end.split('_')[0]),int(end.split('_')[1]),int(end.split('_')[2]))
     df = pd.DataFrame(columns = ['Date', 'User Name', 'Source', 'Source Name', 'Vip Level', 'Risk Level', 'Linked Users', 'Total Bet Amount', 'Total Win Loss Amount', 'Total Turnover Amount', 'GB Bet', 'Lottery Bet', 'Casino Bet', 'IBC Bet', 'Electronic Bet', 'Chess Bet'])
+    maxv = (baren - barbn + 1)
+    bar = progressbar.ProgressBar(max_value=maxv)
+    count = 0
     #列出日期范围
     for i in range((end - begin).days+1):
         day = begin + datetime.timedelta(days=i)
@@ -37,7 +44,10 @@ if __name__ == '__main__':
         open_df = open_csv(csv_date)
         new_df = cleansing_table(open_df, day)
         df = df.append(new_df) 
-        print('Cleasing the csv_file date:'+ csv_date)
+        count += 1 
+        time.sleep(0.1)
+        bar.update(count)
+        #print('Cleasing the csv_file date:'+ csv_date)
     try:
         df.to_csv(r'C:\Users\Jesse\Downloads\Cleasing_data_User Bet Volume_' + csv_date +'.csv', encoding='utf-8-sig', index=False)
         print('Export the file')
